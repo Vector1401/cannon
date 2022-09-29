e4f5d9b2a846fabecc7c7f28fea1181fb4f1fcec
Vector1401 <103223378+Vector1401@users.noreply.github.com>
2022-09-29 19:46:05 -0300

    Update Boat.js

--- PRO-C23-SA-boilerplate-main/js/Boat.js
+++ PRO-C23-SA-boilerplate-main/js/Boat.js
@@ -3,13 +3,13 @@ class Boat {
 
     this.body = Bodies.rectangle(x, y, width, height);
 
-    this.x = x;
-    this.y = y;
+   // this.x = x;
+   //this.y = y;
     this.width = width;
     this.height = height;
     this.boat_image = loadImage("assets/boat.png");
 
-    this.boatPosY = boatPosY;
+    this.boatPositionY = boatPosY;
     World.add(world, this.body);
 
   }
@@ -26,7 +26,7 @@ class Boat {
     push();
     translate(this.body.position.x, this.body.position.y);
     imageMode(CENTER);
-    image(this.boat_image, 0, this.boatPosY, this.width, this.height);
+    image(this.boat_image, 0, this.boatPositionY, this.width, this.height);
     pop();
   }
 }
